179b988c901efa53dd42e116280d6dd778fc7c6b
SircSibaja <79494220+SircSibaja@users.noreply.github.com>
2021-04-18 19:15:25 -0700

    Add files via upload

--- JuegoRPG/main/com/game/Player/Moves.java
+++ JuegoRPG/main/com/game/Player/Moves.java
@@ -1,16 +1,24 @@
 package com.game.Player;
 
+import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Moves extends JPanel implements KeyListener {
 
-	private JLabel Avatar = new JLabel("Este eres tu");
+	private JLabel Avatar = new JLabel();
 
 	public Moves() {
+		ImageIcon imagen = new ImageIcon("E:/Cuarto semestre/POO/Pruebas/Link.jpg");// Se llama la imagen y se crea
+		Icon icono = new ImageIcon(imagen.getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH));// Aqui escogemos el
+																									// tamaño de la
+																									// imagen
+		Avatar.setIcon(icono);
 		addKeyListener(this);
 		setFocusable(true);
 		add(Avatar);
@@ -18,6 +26,7 @@ public class Moves extends JPanel implements KeyListener {
 
 	@Override
 	public void keyTyped(KeyEvent e) {
+
 	}
 
 	@Override
@@ -33,14 +42,12 @@ public class Moves extends JPanel implements KeyListener {
 			Avatar.setLocation(Avatar.getX(), Avatar.getY() + 5);
 		}
 		if (e.getExtendedKeyCode() == KeyEvent.VK_D) {
-			Avatar.setLocation(Avatar.getX() + 5, Avatar.getY() - 5);
+			Avatar.setLocation(Avatar.getX() + 5, Avatar.getY());
 		}
-
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 
 	}
-
 }
